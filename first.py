8e6a4c4ae5f50e62095a80769144fac8095b56db
KiranPandey-data <kiranpande630@gmail.com>
2025-07-04 13:28:19 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,16 @@
+import pyjokes
+
+
+
+# print("printing jokes")
+#this prints random jokes
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+u did it
+
+"""
+# bewakoof
+# aise nhinkarte hai ise 
+# control and forward slash 
